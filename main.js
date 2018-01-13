76539fb8115a7ac86e38f06b2176701ab664a5e8
kellyapaul <34930601+kellyapaul@users.noreply.github.com>
2018-01-13 15:15:10 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+
+var cardTwo = "queen";
+
+var cardThree = "king";
+
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+
+console.log("user flipped " + cardTwo);
+
+console.log("user flipped " + cardThree);
+
+console.log("user flipped " + cardFour);
